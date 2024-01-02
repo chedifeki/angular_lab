b55303402dce1cc0e3ceb430ba10397423d94092
Chedi Feki <chedifeki@gmail.com>
2024-01-02 18:50:38 +0100

    MemberForm and other configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemberComponent } from './member/member.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MemberFormComponent } from './member-form/member-form.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemberComponent
+    MemberComponent,
+    MemberFormComponent
   ],
   imports: [
+    MatTableModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GLOBAL_DB } from '../app.config';
+import { Member } from 'src/Models/Member';
+
 
 @Component({
   selector: 'app-member',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./member.component.css']
 })
 export class MemberComponent {
+  displayedColumns: string[] = ['id','cin', 'name', 'cv', 'type', 'createdDate', 'action'];
+  nom = "chedi";
+  dataSource:Member[] = GLOBAL_DB._DB.members
 
+  source:any[]=[
+    {
+      id:'1254',
+      cin:'125478',
+      name:'chedi',
+      cv:'lien', 
+      type:'student',
+      createdDate:'12/28/2023'
+    },
+    {
+      id:'1475',
+      cin:'789456123',
+      name:'imen',
+      cv:'lien', 
+      type:'teacher',
+      createdDate:'12/28/2023'
+    },
+  ]
 }
