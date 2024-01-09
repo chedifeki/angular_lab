cb8c5af87abd8af122e48e193a4e7345cbe638aa
Chedi Feki <chedifeki@gmail.com>
2024-01-09 20:47:24 +0100

    Add Firebase Auth support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemberComponent } from './member/member.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-
-
-
-import {MatIconModule} from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MemberFormComponent } from './member-form/member-form.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -28,6 +25,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToolListComponent } from './tool-list/tool-list.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { EventListComponent } from './event-list/event-list.component';
+import { FirebaseModule } from 'src/modules/firebase.module';
 
 
 @NgModule({
@@ -59,6 +57,7 @@ import { EventListComponent } from './event-list/event-list.component';
     MatMenuModule,
     MatToolbarModule,
     MatListModule,
+    FirebaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
