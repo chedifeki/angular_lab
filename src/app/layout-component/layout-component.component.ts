46626a0014a93969a4f766b2dd5391c7b37adaf1
Chedi Feki <chedifeki@gmail.com>
2024-01-14 10:06:48 +0100

    Improve login Icon

--- src/app/layout-component/layout-component.component.ts
+++ src/app/layout-component/layout-component.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/Services/auth.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout-component.component.html',
-  styleUrls: ['./layout-component.component.css']
+  styleUrls: ['./layout-component.component.css'],
 })
 export class LayoutComponentComponent {
+  user:any;
 
+  constructor(private AS: AuthService, private router:Router) {
+     this.AS.getUserClaims().then((a)=>{
+      this.user = a;
+      console.log("in layout constructor")
+      console.log(this.user.displayName)
+     })
+  }
+
+  logout() {
+    this.AS.doLogout().then(() => {
+      this.successRedirect();
+    });
+  }
+  successRedirect() {
+      this.router.navigate(['/']);
+  }
 }
